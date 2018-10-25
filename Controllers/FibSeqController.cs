d78d8ba0cbf90a2f29d0c3c2a68180882a92c5e8
johnguggy <42472160+johnguggy@users.noreply.github.com>
2018-10-25 15:14:04 -0400

    Add files via upload

---
+++ Controllers/FibSeqController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FibonacciSequenceMVC2.Controllers
+{
+    public class FibSeqController : Controller
+    {
+        // GET: FibSeq
+        public ActionResult Index()
+        {
+            int[] fib = new int[20];
+            fib[0] = 1;
+            fib[1] = 1;
+            for (int counter = 2; counter <= 19; counter++)
+            {
+                fib[counter] = fib[counter - 2] + fib[counter - 1];
+            }
+            ViewBag.fib = fib;
+            return View();
+        }
+    }
+}
